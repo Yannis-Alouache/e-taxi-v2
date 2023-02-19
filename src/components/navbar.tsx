c0e93b50e86aa2f1a877fe95d066c8151285acef
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-02-19 15:07:35 +0100

    [+] fixed error img

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,11 @@
-import { Fragment, useState } from 'react'
-import { Dialog, Popover, Transition } from '@headlessui/react'
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
 import {
-  ArrowPathIcon,
   Bars3Icon,
-  BookmarkSquareIcon,
-  CalendarIcon,
-  ChartBarIcon,
-  CursorArrowRaysIcon,
-  LifebuoyIcon,
-  PhoneIcon,
-  PlayIcon,
-  ShieldCheckIcon,
-  Squares2X2Icon,
   XMarkIcon,
 } from '@heroicons/react/24/outline'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import Image from 'next/image'
+import navBarLogo from "@/assets/navbar_logo.jpg"
 
 
 const navigation = [
@@ -23,68 +14,6 @@ const navigation = [
   { name: 'Marketplace', href: '#' },
   { name: 'Company', href: '#' },
 ]
-
-const solutions = [
-  {
-    name: 'Analytics',
-    description: 'Get a better understanding of where your traffic is coming from.',
-    href: '#',
-    icon: ChartBarIcon,
-  },
-  {
-    name: 'Engagement',
-    description: 'Speak directly to your customers in a more meaningful way.',
-    href: '#',
-    icon: CursorArrowRaysIcon,
-  },
-  { name: 'Security', description: "Your customers' data will be safe and secure.", href: '#', icon: ShieldCheckIcon },
-  {
-    name: 'Integrations',
-    description: "Connect with third-party tools that you're already using.",
-    href: '#',
-    icon: Squares2X2Icon,
-  },
-  {
-    name: 'Automations',
-    description: 'Build strategic funnels that will drive your customers to convert',
-    href: '#',
-    icon: ArrowPathIcon,
-  },
-]
-const callsToAction = [
-  { name: 'Watch Demo', href: '#', icon: PlayIcon },
-  { name: 'Contact Sales', href: '#', icon: PhoneIcon },
-]
-const resources = [
-  {
-    name: 'Help Center',
-    description: 'Get all of your questions answered in our forums or contact support.',
-    href: '#',
-    icon: LifebuoyIcon,
-  },
-  {
-    name: 'Guides',
-    description: 'Learn how to maximize our platform to get the most out of it.',
-    href: '#',
-    icon: BookmarkSquareIcon,
-  },
-  {
-    name: 'Events',
-    description: 'See what meet-ups and other events we might be planning near you.',
-    href: '#',
-    icon: CalendarIcon,
-  },
-  { name: 'Security', description: 'Understand how we take your privacy seriously.', href: '#', icon: ShieldCheckIcon },
-]
-const recentPosts = [
-  { id: 1, name: 'Boost your conversion rate', href: '#' },
-  { id: 2, name: 'How to use search engine optimization to drive traffic to your site', href: '#' },
-  { id: 3, name: 'Improve your customer experience', href: '#' },
-]
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ')
-}
 
 
 export default function Navbar() {
@@ -97,7 +26,7 @@ export default function Navbar() {
       <div className="flex lg:flex-1">
         <a href="#" className="-m-1.5 p-1.5">
           <span className="sr-only">Your Company</span>
-          <img className="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+          <Image className="h-8" src={navBarLogo} width={32} height={32} alt="" />
         </a>
       </div>
       <div className="flex lg:hidden">
--- src/components/testimonials.tsx
+++ src/components/testimonials.tsx
@@ -5,19 +5,22 @@ const testimonials = [
         text: '“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero ad tempora doloribus expedita laborum saepe voluptas perferendis delectus assumenda rerum, culpa aperiam dolorum, obcaecati corrupti aspernatur a.”.',
         img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
         name: "Robert",
-        work_function: "CTO, Robert Consultency"
+        work_function: "CTO, Robert Consultency",
+        key: "0"
     },
     {
         text: '“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero ad tempora doloribus expedita laborum saepe voluptas perferendis delectus assumenda rerum, culpa aperiam dolorum, obcaecati corrupti aspernatur a.”.',
         img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
         name: "Robert",
-        work_function: "CTO, Robert Consultency"
+        work_function: "CTO, Robert Consultency",
+        key: "1"
     },
     {
         text: '“Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quibusdam ducimus libero ad tempora doloribus expedita laborum saepe voluptas perferendis delectus assumenda rerum, culpa aperiam dolorum, obcaecati corrupti aspernatur a.”.',
         img: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
         name: "Robert",
-        work_function: "CTO, Robert Consultency"
+        work_function: "CTO, Robert Consultency",
+        key: "2"
     },
 
 ]
@@ -43,20 +46,20 @@ function Testimonials() {
 
                     {testimonials.map((testimonial) => {
                         return (
-                            <div className="p-8 border rounded-lg border-amber-400">
-                            <p className="leading-loose text-gray-500">
-                                {testimonial.text}
-                            </p>
-    
-                            <div className="flex items-center mt-8 -mx-2">
-                                <Image className="object-cover mx-2 rounded-full w-14 shrink-0 h-14 ring-4 ring-gray-300 dark:ring-gray-700" src={testimonial.img} width={880} height={880} alt="" />
-    
-                                <div className="mx-2">
-                                    <h1 className="font-semibold text-gray-800">{testimonial.name}</h1>
-                                    <span className="text-sm text-gray-500">{testimonial.work_function}</span>
+                            <div className="p-8 border rounded-lg border-amber-400" key={testimonial.key}>
+                                <p className="leading-loose text-gray-500">
+                                    {testimonial.text}
+                                </p>
+        
+                                <div className="flex items-center mt-8 -mx-2">
+                                    <Image className="object-cover mx-2 rounded-full w-14 shrink-0 h-14 ring-4 ring-gray-300 dark:ring-gray-700" src={testimonial.img} width={880} height={880} alt="" />
+        
+                                    <div className="mx-2">
+                                        <h1 className="font-semibold text-gray-800">{testimonial.name}</h1>
+                                        <span className="text-sm text-gray-500">{testimonial.work_function}</span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                         )
                     })}
 
