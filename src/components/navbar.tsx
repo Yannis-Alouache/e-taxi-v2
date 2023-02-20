b798947355734287206a4ddbbadfb35d8f6243d8
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-02-20 13:01:11 +0100

    [-] removed login link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -47,9 +47,6 @@ export default function Navbar() {
         ))}
       </div>
       <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-        <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-          Log in <span aria-hidden="true">&rarr;</span>
-        </a>
       </div>
     </nav>
     <Dialog as="div" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
@@ -80,14 +77,6 @@ export default function Navbar() {
                   {item.name}
                 </a>
               ))}
-            </div>
-            <div className="py-6">
-              <a
-                href="#"
-                className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
-              >
-                Log in
-              </a>
             </div>
           </div>
         </div>
