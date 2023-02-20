48b8449f005a3db94f2460716297bc1768bc9875
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-02-20 14:47:24 +0100

    [~] fixed mobile menu && added icon to monile menu && added link to section

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,43 +5,40 @@ import {
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 import Image from 'next/image'
-import navBarLogo from "@/assets/navbar_logo.jpg"
+import navBarLogo from "@/assets/navbar_logo.png"
 
 
 const navigation = [
-  { name: 'Product', href: '#' },
-  { name: 'Features', href: '#' },
-  { name: 'Marketplace', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Accueil', href: '#' },
+  { name: 'Reservation', href: '#bookingBanner' },
+  { name: 'Services', href: '#services' },
+  { name: 'Véhicule', href: '#carDetail' },
+  { name: 'Avis Client', href: '#testimonials' },
 ]
 
 
 export default function Navbar() {
 
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
-  
+
+
   return (
     <div className="px-6 pt-6 lg:px-8 bg-transparent sticky z-50">
     <nav className="flex items-center justify-between" aria-label="Global">
       <div className="flex lg:flex-1">
-        <a href="#" className="-m-1.5 p-1.5">
-          <span className="sr-only">Your Company</span>
-          <Image className="h-8" src={navBarLogo} width={32} height={32} alt="" />
-        </a>
       </div>
-      <div className="flex lg:hidden">
+      <div className={mobileMenuOpen === true ? 'hidden' : 'flex' + " lg:hidden"}>
         <button
           type="button"
           className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white-700"
           onClick={() => setMobileMenuOpen(true)}
         >
-          <span className="sr-only">Open main menu</span>
           <Bars3Icon className="h-6 w-6" aria-hidden="true" />
         </button>
       </div>
       <div className="hidden lg:flex lg:gap-x-12">
         {navigation.map((item) => (
-          <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-white-900">
+          <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-white-900 hover:underline">
             {item.name}
           </a>
         ))}
@@ -53,15 +50,14 @@ export default function Navbar() {
       <Dialog.Panel className="fixed inset-0 z-10 overflow-y-auto bg-white px-6 py-6 lg:hidden">
         <div className="flex items-center justify-between">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img className="h-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+            <span className="sr-only">E-Taxi-Lille</span>
+            <Image className="h-8 w-full" src={navBarLogo} alt="" />
           </a>
           <button
             type="button"
             className="-m-2.5 rounded-md p-2.5 text-gray-700"
             onClick={() => setMobileMenuOpen(false)}
           >
-            <span className="sr-only">Close menu</span>
             <XMarkIcon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
