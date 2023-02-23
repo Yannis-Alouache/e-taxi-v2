75b2b924823ffac4b5dc3ce0b05c9998448a22a2
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-02-23 22:15:33 +0100

    [+] added lg breakpoint responsivness

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -41,18 +41,18 @@ function Services() {
   return (
     <>
         <div id='services' className='mb-32 mt-24'>
-            <div className='container mx-auto px-40 py-10'>
+            <div className='container mx-auto lg:px-0 px-40 py-10'>
                 <div className="text-center">
-                    <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl xl:mb-20">Nos Services</h2>
+                    <h2 className="text-3xl font-bold tracking-tight lg:mb-10 text-gray-900 sm:text-4xl xl:mb-20">Nos Services</h2>
                     
-                    <div className="grid xl:grid-cols-4 sm:grid-cols-1">
+                    <div className="grid xl:grid-cols-4 lg:grid-cols-2 sm:grid-cols-1">
                         {services.map((service) => {
                             return (
                                 <div key={service.key} className={styles.withSeparator}>
-                                    <div className="relative w-24 h-24 mx-auto">
+                                    <div className="relative w-24 h-24 mx-auto lg:mb-2">
                                         <Image src={service.image} alt={service.alt} fill={true} />
                                     </div>
-                                    <h3 className='text-gray-900 text-xl xl:mt-10 xl:mb-3 uppercase font-bold'>{service.title}</h3>
+                                    <h3 className='text-gray-900 text-xl lg:mb-3 xl:mt-10 xl:mb-3 uppercase font-bold'>{service.title}</h3>
                                     <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800 px-4">
                                         I will be the leader of a company that ends up being worth billions
                                         of dollars, because I got the answers. I understand culture. I am
