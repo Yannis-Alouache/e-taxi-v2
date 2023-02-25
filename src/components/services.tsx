c1532e2e7170f614ef5fb016dc1f3993816a480b
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-02-25 20:18:15 +0100

    [+] added mobile responsivity

--- src/components/services.tsx
+++ src/components/services.tsx
@@ -40,8 +40,8 @@ const services = [
 function Services() {
   return (
     <>
-        <div id='services' className='mb-32 sm:mb-20 mt-24'>
-            <div className='container mx-auto lg:px-6 md:px-6 sm:px-6 px-40 py-10'>
+        <div id='services' className='mb-16 sm:mb-20 sm:mt-24 mt-10'>
+            <div className='container mx-auto lg:px-6 md:px-6 sm:px-6 px-5 py-10'>
                 <div className="text-center">
                     <h2 className="text-3xl font-bold tracking-tight md:mb-4 lg:mb-10 text-gray-900 sm:text-4xl sm:mb-8 xl:mb-20">Nos Services</h2>
                     
@@ -49,10 +49,10 @@ function Services() {
                         {services.map((service) => {
                             return (
                                 <div key={service.key} className={styles.withSeparator}>
-                                    <div className="relative w-24 h-24 mx-auto md:mb-4 lg:mb-2 sm:mt-5 sm:mb-5">
+                                    <div className="relative w-24 h-24 mx-auto md:mb-4 lg:mb-2 sm:mt-5 sm:mb-5 mt-8 mb-4">
                                         <Image src={service.image} alt={service.alt} fill={true} />
                                     </div>
-                                    <h3 className='text-gray-900 text-xl sm:mb-1 md:mb-1 lg:mb-3 xl:mt-10 xl:mb-3 uppercase font-bold'>{service.title}</h3>
+                                    <h3 className='text-gray-900 text-mb sm:text-xl sm:mb-1 md:mb-1 lg:mb-3 xl:mt-10 xl:mb-3 uppercase font-bold'>{service.title}</h3>
                                     <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800 px-4">
                                         I will be the leader of a company that ends up being worth billions
                                         of dollars, because I got the answers. I understand culture. I am
