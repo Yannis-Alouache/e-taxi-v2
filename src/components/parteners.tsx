2457e9590547cf90617bf7e5025f61c5515a4a0c
Yannis-Alouache <alouacehyannis1@gmail.com>
2023-02-25 20:25:01 +0100

    [+] added mobile responsivity

--- src/components/parteners.tsx
+++ src/components/parteners.tsx
@@ -12,17 +12,17 @@ const parteners = [
 function Parteners() {
   return (
     <div id='parteners' className='mt-10 bg-amber-400'>
-        <div className='xl:container xl:mx-auto md:px-5 lg:px-10 sm:px-5 py-10'>
-            <div className="grid xl:grid-cols-6 sm:grid-cols-3 sm:gap-3 md:grid-cols-6 lg:grid-cols-6 items-center">
+        <div className='xl:container xl:mx-auto md:px-5 lg:px-10 sm:px-5 py-10 px-5'>
+            <div className="grid xl:grid-cols-6 sm:grid-cols-3 sm:gap-3 gap-3 md:grid-cols-6 lg:grid-cols-6 grid-cols-3 items-center">
 
                 <div>
-                    <h3 className='text-gray-900 text-xl md:text-sm uppercase font-bold'>Nos Partenaires</h3>
-                    <h3 className='text-white-900 text-3xl md:text-2xl uppercase font-bold'>Et Clients</h3>
+                    <h3 className='text-gray-900 text-md md:text-sm lg:text-md uppercase font-bold'>Nos Partenaires</h3>
+                    <h3 className='text-white-900 text-md md:text-2xl lg:text-2xl uppercase font-bold'>Et Clients</h3>
                 </div>
                 {parteners.map((partener) => {
                 return (
                     <div key={partener.id}>
-                        <div className="relative lg:w-24 lg:h-24 md:w-20 md:h-20 sm:w-20 sm:h-20 mx-auto">
+                        <div className="relative lg:w-24 lg:h-24 md:w-20 md:h-20 sm:w-20 sm:h-20 w-16 h-16 mx-auto">
                             <Image src={require("@/assets/" + partener.image)} alt={partener.alt} fill={false} className="absolute top-1/2 -translate-y-1/2" />
                         </div>
                     </div>
